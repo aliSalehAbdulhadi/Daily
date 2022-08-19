fff0e000bd68208015097b8ed26037863987df84
aliSalehAbdulhadi <iqalisaleh@gmail.com>
2022-08-19 21:47:00 +0300

    test

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,11 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import signUpSlice from "../slices/authentication/signUpSlice";
-import signInSlice from "../slices/authentication/signInSlice";
-import userSlice from "../slices/authentication/userSlice";
-import addTodoSlice from "../slices/features/addTodoSlice";
-import getTodoSlice from "../slices/features/getTodoSlice";
-import darkMode from "../slices/features/darkMode";
-import resetPasswordSlice from "../slices/authentication/resetPasswordSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import signUpSlice from '../slices/authentication/signUpSlice';
+import signInSlice from '../slices/authentication/signInSlice';
+import userSlice from '../slices/authentication/userSlice';
+import addTodoSlice from '../slices/features/addTodoSlice';
+import getTodoSlice from '../slices/features/getTodoSlice';
+import darkMode from '../slices/features/darkMode';
+import resetPasswordSlice from '../slices/authentication/resetPasswordSlice';
 
 const store = configureStore({
   reducer: {
