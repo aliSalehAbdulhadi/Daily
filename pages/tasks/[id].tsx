d3a28d8139a49ffae28689c27d1ddfd659f3afbb
aliSalehAbdulhadi <iqalisaleh@gmail.com>
2022-09-22 12:41:28 +0300

    moved milestone form all the way to the top in mobile screen

--- pages/tasks/[id].tsx
+++ pages/tasks/[id].tsx
@@ -130,13 +130,13 @@ const MileStone = () => {
   return (
     <div
       ref={scrollRefTop}
-
-      className={`flex flex-col text-sm font-Comfortaa w-full min-h-[90vh] ${
+      className={`flex flex-col text-sm font-Comfortaa w-full ${
+        openAdvancedForm ? 'h-[90vh] overflow-hidden' : 'min-h-[90vh]'
+      } ${
         dark ? 'bg-primaryColor' : 'bg-secondaryLight'
       }  text-white  transition-all`}
     >
       <div className={`${openAdvancedForm ? 'hidden' : 'block'}`}>
-
         <div
           className={`flex flex-col items-center justify-center w-full border-b-[1px] sticky top-0 z-[50]`}
         >
@@ -175,7 +175,6 @@ const MileStone = () => {
             className={`flex w-full shadow-lg ${
               dark ? 'bg-primaryColor' : 'bg-secondaryLight'
             }   ${task?.milestones.length === 0 ? 'pt-0' : 'pt-3'}`}
-
           >
             <div
               className={`w-full px-3 pb-3 mr-1   ${
@@ -186,7 +185,6 @@ const MileStone = () => {
             </div>
           </div>
         </div>
-
 
         <div className={` flex flex-col w-full`}>
           {milestonesSortHandler()?.map((milestone: any, i) => {
@@ -212,7 +210,6 @@ const MileStone = () => {
             }}
             className={`sticky w-fit bottom-0 mt-5 ml-2 py-3 z-30 flex flex-col items-center justify-center self-center cursor-pointer ${
               openAdvancedForm ? 'hidden' : 'block'
-
             }`}
           >
             <h1
@@ -243,10 +240,9 @@ const MileStone = () => {
             )}
           </div>
         </div>
-
       </div>
       <div
-        className={`quillFormEnterAnimationMobile absolute z-50  top-[5.2rem] ${
+        className={`quillFormEnterAnimationMobile absolute z-50  top-0 ${
           dark ? 'bg-primaryColor' : 'bg-secondaryLight'
         }   ${openAdvancedForm ? 'block' : 'hidden'} `}
         ref={milestoneAdvancedFormRef}
@@ -256,7 +252,6 @@ const MileStone = () => {
           taskId={String(task?.id)}
           setScroll={setScroll}
         />
-
       </div>
     </div>
   );
