17a7ec4e4fe9cdcda6272e629f56a014b453b5c7
aliSalehAbdulhadi <iqalisaleh@gmail.com>
2022-10-16 19:58:42 +0300

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withPWA({});
