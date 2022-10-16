b12a35d49bbe7905558dc97aeb9b83eb027991f2
aliSalehAbdulhadi <iqalisaleh@gmail.com>
2022-10-16 21:29:59 +0300

    fixed pwa not working

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,48 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="manifest" href="/manifest.json"></link>
-          <link rel="daily-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#fff"></meta>
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="application-name" content="Daily" />
+          <meta name="apple-mobile-web-app-title" content="Daily" />
+          <meta name="theme-color" content="#fff" />
+          <meta name="msapplication-navbutton-color" content="#fff" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <meta name="msapplication-starturl" content="/" />
+          <link
+            rel="apple-touch-icon"
+            type="png"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="png"
+            sizes="192x192"
+            href="/icon-maskable-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="png"
+            sizes="512x512"
+            href="/icon-maskable-512x512.png"
+          />
+
+          <link
+            rel="icon"
+            type="png"
+            sizes="512x512"
+            href="/icon-512x512.png"
+          />
+          <link
+            rel="icon"
+            type="png"
+            sizes="192x192"
+            href="/icon-192x192.png"
+          />
         </Head>
         <body>
           <Main />
