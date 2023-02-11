65622ccf383db7b6acea001c39f3ec1be4ac39f2
aliSalehAbdulhadi <iqalisaleh@gmail.com>
2023-02-11 13:57:21 +0300

    changed google sign in text color and add title to the button

--- src/components/googleButton/GoogleButton.tsx
+++ src/components/googleButton/GoogleButton.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 const GoogleButton = () => {
   return (
-    <div className=" rounded bg-white  text-primaryColor hover:text-secondaryColor cursor-pointer transition-all">
+    <div
+      title="Google sign in"
+      className=" rounded bg-white cursor-pointer transition-all"
+    >
       <div className="flex flex-row items-center justify-items-center">
         <div className="flex items-center  justify-center mobileTaskCardBoxShadow">
           <Image
@@ -13,7 +16,7 @@ const GoogleButton = () => {
             alt="Google Icon"
           />
         </div>
-        <span className="pr-2 font-bold   text-xs py-1 px-3 opacity-90">
+        <span className="pr-2 font-bold  text-gray-800  text-xs py-1 px-3 opacity-80 hover:opacity-[.85]">
           Sign in with Google
         </span>
       </div>
